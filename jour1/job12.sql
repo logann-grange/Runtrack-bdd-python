94ae772cffa402ef4651cd78c249c477d99fc607
logann-grange <logann.grange@laplateforme.io>
2026-03-02 15:07:54 +0100

    commit du jour1

---
+++ jour1/job12.sql
@@ -0,0 +1,3 @@
+INSERT INTO etudiant (id, nom, prenom, age, email) VALUES (6, "Dupuis", "Martin", 18, "martin.dupuis@laplateforme.io");
+
+SELECT * FROM etudiant WHERE nom = "Dupuis";
---
+++ jour1/job13.sql
@@ -0,0 +1,1 @@
+SELECT * FROM etudiant WHERE age <= 25 AND age >=18;
